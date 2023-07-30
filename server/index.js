ea15cd9d7cc9239a0290a9b9156def849d57c0e3
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-07-30 16:40:35 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -142,10 +142,10 @@ app.get("/post/:id", async (req, res) => {
 
 if (process.env.NODE_ENV == "production") {
   const path = require("path");
-  app.use(express.static(path.join(__dirname, "./client/build")));
-  app.get("*", function (req, res) => {
+  app.use(express.static(path.resolve(__dirname, "./client/build")));
+  app.get("*", (req, res) => {
     
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
   });
 }
 
