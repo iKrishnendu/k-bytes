4bafb7a0ea952181d3049a0d43c3f8e5e3f43a72
Krishnendu Sahoo <102577456+iKrishnendu@users.noreply.github.com>
2023-07-30 21:27:16 +0530

    Update Home.js

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -28,7 +28,7 @@ const Home = () => {
         </p>
       </div>
       <div className="mt-8 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 justify-center ">
-        {posts.length > 0 && posts.map((post) => <PostCard {...post} />)}
+        {posts.length > 0 && posts.map((post) => <PostCard  key={post._id} {...post} />)}
       </div>
     </>
   );
